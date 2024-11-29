c98b200714d784f0577067633887812d897c100b
xxxsanechekxxx <xuangzhouqq1@gmail.com>
2024-11-29 10:01:01 +0700

    delete

--- src/components/items/items.tsx
+++ src/components/items/items.tsx
@@ -15,13 +15,9 @@ const Items = () => {
             <div>
                 <Item brand="Very Sexy" img={panty} name="Shine Strap Rose Lace Crotchless Brazilian Panty" color="Campari Red" quantity={1} price={12.45}
                       size="S"/>
-                <Item brand="Very Sexy" img={bra} name="The Fabulous by Victoria's Secret Full-Cup Shine Strap Rose Lace Bra" color="Campari Red" quantity={1} price={34.99}
-                      size="34D"/>
-                <Item brand="Victoria's Secret" img={bralette} name="BODYWEAR by Victoria with FeatherSoft™ Innovation Bralette" color="Coconut White" quantity={1} price={14.45}
-                      size="S"/>
             </div>
             <div>
-                <Total sum={61.89} />
+                <Total sum={12.45} />
             </div>
         </>
     )
